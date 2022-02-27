f47d9ae9edf4642b256fc0cd581e1091b28d3ce3
Kunvar13 <kunvar13@gmail.com>
2022-02-27 00:39:02 -0600

    Added Category to the library

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcLibrary.Data;
+using MvcLibrary.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,31 @@ namespace MvcLibrary.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            IEnumerable<Category> objList = _db.Categories;
+            return View(objList);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
