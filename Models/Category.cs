909ca1c0ae4bfabe08ffb32beec4051e2a820200
Kalpesh Kunvar <KalpeshK@KalPLaptop.corp.ampmservice.com>
2022-02-26 21:17:31 -0600

    Added database Library and then added Category table in it

---
+++ Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcLibrary.Models
+{
+    public class Category
+    {
+        [Key]
+        public int id { get; set; }
+
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+    }
+}
